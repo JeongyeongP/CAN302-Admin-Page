a97559d239fc49cb828f5f17fcbd3579b918c96a
christinasoen <103727509+tinasoen@users.noreply.github.com>
2023-05-02 03:09:43 +0700

    Updated Config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 
 // initializing variables
 $username = "";
@@ -9,118 +9,91 @@ $errors = array();
 // connect to the database
 $conn = mysqli_connect('localhost', 'root', '', 'can302');
 
-function mypost($str) {
-  $val = !empty($_POST[$str]) ? $_POST[$str] : '';
-  return $val;
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
 }
 
-//receive query parameters
-$first_name = mypost('first_name');
-$last_name = mypost('last_name');
-$email = mypost('email');
-$password = mypost('password');
-$cpassword = mypost('cpassword');
-$phone = mypost('phone');
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
-
-  
-  if (!empty($email) && !empty($first_name) && !empty($last_name) && !empty($password)){
-
-    if ($password == $cpassword) {
-      $password = md5($password);
-      $query = "insert into `user` (`user_id`, `user_email`, `first_name`, `last_name`, `password`, `phone_number`) values('0', '$email', '$first_name', '$last_name', '$password', '$phone')";
-      mysqli_query($conn, $query);
-      echo '<script language="javascript">';
-      echo 'alert("Successfully Registered!")';
-      echo '</script>';
-      header('location: login.php');
-    } else {
-      echo '<script language="javascript">';
-      echo 'alert("Passwords do not match")';
-      echo '</script>';
-    }
-  
-  
-  }
+  // receive all input values from the form
+  $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
+  $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $password = mysqli_real_escape_string($conn, $_POST['password']);
+  $cpassword= mysqli_real_escape_string($conn, $_POST['cpassword']);
+  $is_admin = (int)mysqli_real_escape_string($conn, $_POST['is_admin']);
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
-  // if (empty($first_name)) { array_push($errors, "First name is required");}
-  // if (empty($last_name)) { array_push($errors, "Last name is required"); }
-  // if (empty($email)) { array_push($errors, "Email is required"); }
-  // if (empty($password)) { array_push($errors, "Password is required"); }
-  // if ($password != $cpassword) {
-	//   array_push($errors, "The two passwords do not match");
-  // }
+  if (empty($first_name)) { array_push($errors, "First is required"); }
+  if (empty($last_name)) { array_push($errors, "Last is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password)) { array_push($errors, "Password is required"); }
+  if ($password != $cpassword) {
+	array_push($errors, "The two passwords do not match");
+  }
 
-  // // first check the database to make sure 
-  // // a user does not already exist with the same email
-  // $user_check_query = "SELECT * FROM user WHERE email='$email' LIMIT 1";
-  // $result = mysqli_query($conn, $user_check_query);
-  // $user = mysqli_fetch_assoc($result);
+  // first check the database to make sure 
+  // a user does not already exist with the same email
+  $user_check_query = "SELECT * FROM user_regist WHERE email='$email' LIMIT 1";
+  $result = mysqli_query($conn, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
   
-  // if ($user) { // if user exists
-  //   if ($user['email'] === $email) {
-  //     array_push($errors, "Email already exists");
-  //   }
-  // }
+  if ($user) { // if user exists
+    if ($user['email'] === $email) {
+      array_push($errors, "Email already exists");
+    }
+  }
 
-  // // Finally, register user if there are no errors in the form
-  // if (count($errors) == 0) {
-  // 	$password = md5($password);//encrypt the password before saving in the database
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password);//encrypt the password before saving in the database
+  	$query = "INSERT INTO user_regist (first_name, last_name, email, password, is_admin) 
+  			  VALUES('$first_name', '$last_name', '$email', '$password', '$is_admin')";
 
-  // 	$query = "insert into user (`user_id`, `user_email`, `first_name`, `last_name`, `password`) values('0', '$email', '$first_name', '$last_name', '$password')";
-  //   mysqli_query($conn, $query);
-  // 	// if (mysqli_query($conn, $query)){
-  //   //   $_SESSION['email'] = $email;
-  //   //   $_SESSION['success'] = "Please Log In!";
-  //   //   header('location: login_page.php');
-  //   // }
-
-
-  // }
+  	mysqli_query($conn, $query);
+  	#$_SESSION['email'] = $email;
+  	$_SESSION['success'] = "Please Log In!";
+    #$_SESSION['user_type'] = ""
+  	header('location: login_page.php');
+  }
 }
 
 // LOGIN USER
 if (isset($_POST['login_user'])) {
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-
-  $password = md5($password);
-  $query = "select * from user where user_email='$email' and password='$password'";
-  $results = mysqli_query($conn, $query);
-  if (mysqli_num_rows($results) == 1) {
-    $_SESSION['email'] = $email;
-    $_SESSION['success'] = "You are now logged in";
-    header('location: dashboard.php');
-  } else {
-    $error = "Your Login Name or Password is invalid";
+  if (empty($email)) {
+  	array_push($errors, "Email is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
   }
 
-  
 
-  // $email = mysqli_real_escape_string($conn, $_POST['email']);
-  // $password = mysqli_real_escape_string($conn, $_POST['password']);
-
-  // if (empty($email)) {
-  // 	array_push($errors, "Email is required");
-  // }
-  // if (empty($password)) {
-  // 	array_push($errors, "Password is required");
-  // }
-
-  // if (count($errors) == 0) {
-  // 	$password = md5($password);
-  // 	$query = "SELECT * FROM user_regist WHERE email='$email' AND password='$password'";
-  // 	$results = mysqli_query($conn, $query);
-  // 	if (mysqli_num_rows($results) == 1) {
-  // 	  $_SESSION['email'] = $email;
-  // 	  $_SESSION['success'] = "You are now logged in";
-  // 	  header('location: dashboard.php');
-  // 	}else {
-  // 		array_push($errors, "Wrong username/password combination");
-  // 	}
-  // }
+  if (count($errors) == 0) {
+  	$hash = md5($password);
+  	$query = "SELECT * FROM user_regist WHERE email='$email' AND password='$hash' LIMIT 1";
+  	$results = mysqli_query($conn, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['email'] = $email;
+  	  $_SESSION['success'] = "You are now logged in";
+      $row = mysqli_fetch_assoc($results);
+      $_SESSION["name"] = $row['first_name'] . " " . $row['last_name'];
+      $is_admin = $row['is_admin'];
+      $_SESSION['is_admin'] = $is_admin;
+      if ($is_admin == 1) {
+        header('location: admin_profile.php');
+      } else {
+        array_push($errors, "You are a User, not an Admin!");
+      }
+  	} else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
 }
 
 ?>
@@ -132,5 +105,3 @@ if (isset($_POST['login_user'])) {
   	<?php endforeach ?>
   </div>
 <?php  endif ?>
-
-
