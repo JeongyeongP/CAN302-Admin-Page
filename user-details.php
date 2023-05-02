9a4c2134031e81239f9ece1bbc7577265a0ea51d
thheea <theafedraa@gmail.com>
2023-05-03 00:00:45 +0700

    profile.css outside css folder

--- user-details.php
+++ user-details.php
@@ -303,15 +303,7 @@
                 <!-- Coupon End -->
             <!-- End of Main Content -->
                                  </div>
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-               <div class="container my-auto">
-                  <div class="copyright text-center my-auto">
-                     <span>Copyright &copy; Your Website 2020</span>
-                  </div>
-               </div>
-            </footer>
-            <!-- End of Footer -->
+ 
          </div>
          <!-- End of Content Wrapper -->
       </div>
