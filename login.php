390b793e21c474cbf81e788b985129ca89427c4d
JeongyeongP <100497491+JeongyeongP@users.noreply.github.com>
2023-05-02 09:41:31 +0800

    Login-Register Pages UI Modify

--- login.php
+++ login.php
@@ -17,57 +17,46 @@
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">  
 </head>
-    
-    
 <body>
-    <section class="vh-100">
-      <div class="container-fluid">
-
-        <!-- Left-->
-        <div class="row">
-          <div class="col-sm-6 px-0 d-none d-sm-block color= " >
-              <img src="images/background.png"
-                alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
-          </div>
-
-          <!-- Right -->
-            <div class="d-flex align-items-center px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-              <form class="width: 23rem; user justify-content-center"  method="POST">
-                  <h1 class="h4 text-gray-900 mb-4 font-weight-bold">Welcome!</h1>
-                  
-                  <!-- Email -->
-                  <div class="form-group mb-4">
-                      <input type="email" id="email" required placeholder="Email Address" class="form-control form-control-lg" />
-                  </div>
-                  <!-- Password -->
-                  <div class="form-group mb-4">
-                      <input type="password" id="password" required placeholder="Password" class="form-control form-control-lg" />
-                  </div>
-                  <p class="small text-right"><a class="text-muted" href="#">Forgot password?</a></p>
-                  <!-- Login Button -->
-                  <div class="pt-1 mb-4">
-                    <!-- <a href="#" class="btn btn-info btn-lg btn-block" name="login_user" >
-                      Login
-
-
-                  </a> -->
-
-                  <button class="btn btn-info btn-lg btn-block" name="login_user" id="login_user" type="submit"><a href="dashboard.php">Login</a></button>
-                  </div>
-                  <!-- Register Account -->
-                  <div class="text-center">
-                      You don't have an account?
-                      <a href="register.php" class="register-link"> Register here! </a>
-                  </div>
-              </form>
-            </div>    
+  <section class="vh-100">
+    <div class="login-contents">
+      <!-- Left-->
+      <div class="row">
+        <div class="col-sm-4 px-50 d-none d-sm-block" style= "background-color: white;">
+          <img src="images/background.png"
+            alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
         </div>
-          
+
+        <!-- Right -->
+        <div class="d-flex align-items-center px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5" style="margin-left: 180px;">
+
+          <form class="login-form width: 23rem; user justify-content-center" method="POST" style="border-radius: 10px;">
+            <h1 class="h4 text-gray-900 mb-4 font-weight-bold" style="margin-left: 200px;">Welcome!</h1>
+            <!-- Email -->
+            <div class="form-group mb-4">
+              <input type="email" id="email" style="width: 522px;" required placeholder="Email Address" class="form-control form-control-lg" />
+            </div>
+            <!-- Password -->
+            <div class="form-group mb-4">
+              <input type="password" id="password" required placeholder="Password" class="form-control form-control-lg" />
+            </div>
+            <p class="small text-right"><a class="text-muted" href="#">Forgot password?</a></p>
+            <!-- Login Button -->
+            <div class="pt-1 mb-4">
+              <button class="btn btn-info btn-lg btn-block" style="background-color: #96A1FE; color: white;" name="login_user" id="login_user" type="submit"><a href="dashboard.php">Login</a></button>
+            </div>
+            <!-- Register Account -->
+            <div class="text-center">
+              You don't have an account?
+              <a href="register.php" class="register-link"> Register here! </a>
+            </div>
+          </form>
+        </div>
       </div>
-   </section>
-    
+    </div>
+  </section>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Super Market</title>
 
    <!-- Custom fonts for this template-->
 
@@ -17,60 +16,57 @@
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
-    <header>
-    <!--Logo-->
-        <div class="sidebar-brand-text mx-3"><img src="images/supermarket.png" width="150px"></div>    
-    </header>
+  
     
 </head>
 <body>
     <section class="vh-100">
-      <div class="container-fluid">
+      <div class="contents-register">
         <div class="row">
 
           <!--Left-->
-          <div class="col-sm-6 px-0 d-none d-sm-block color= " >
+          <div class="col-sm-4 px-50 d-none d-sm-block" style= "background-color: white;">
               <img src="images/background.png"
                 alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
           </div>
           
           <!-- Right -->   
           
-            <div class="d-flex align-items-center px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+            <div class="d-flex align-items-center px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5" style="margin-left: 180px;">
 
               <form class="width: 23rem; user justify-content-center" action="" method="POST">
     
                   <h1 class="h4 text-gray-900 mb-4 font-weight-bold">Register an Account!</h1>
     
           
-                    <input type="first_name" id="first_name" name="first_name" required placeholder="First Name" class="form-control form-control-lg" />
+                    <input type="first_name" id="first_name" name="first_name" style="width: 522px; margin-bottom: 5px;" required placeholder="First Name" class="form-control form-control-lg" />
              
 
           
-                    <input type="last_name" id="last_name" name="last_name" required placeholder="Last Name" class="form-control form-control-lg" />
+                    <input type="last_name" id="last_name" name="last_name" style="width: 522px; margin-bottom: 5px;" required placeholder="Last Name" class="form-control form-control-lg" />
                
 
      
-                    <input type="email" id="email" name="email"  required placeholder="Email Address" class="form-control form-control-lg" />
+                    <input type="email" id="email" name="email" style="width: 522px; margin-bottom: 5px;" required placeholder="Email Address" class="form-control form-control-lg" />
     
-                      <input type="password" id="password" name="password" required placeholder="Password" class="form-control form-control-lg" />
+                      <input type="password" id="password" name="password" style="width: 522px; margin-bottom: 5px;" required placeholder="Password" class="form-control form-control-lg" />
           
 
          
-                    <input type="password" id="cpassword" name="cpassword" required placeholder="Confirm Password" class="form-control form-control-lg" />
-                    <input type="tel" id="phone" name="phone" required placeholder="Phone Number" class="form-control form-control-lg" />
+                    <input type="password" id="cpassword" name="cpassword" style="width: 522px; margin-bottom: 5px;" required placeholder="Confirm Password" class="form-control form-control-lg" />
+                    <input type="tel" id="phone" name="phone" style="width: 522px; margin-bottom: 5px;" required placeholder="Phone Number" class="form-control form-control-lg" />
             
 
                   <!--Select type of user-->
                   <div>
-                    <select name="is_admin" id="is_admin">
+                    <select name="is_admin" id="is_admin" style="width: 522px; margin-bottom: 5px;">
                         <option value="user">user</option>
                         <option value="admin">admin</option>
                     </select>
                   </div>
       
               
-                      <button class="btn btn-info btn-lg btn-block" name="reg_user" id="reg_user" type="submit">Register</button>
+                      <button class="btn btn-info btn-lg btn-block"  style="background-color: #96A1FE; color: white; margin-bottom: 5px;" name="reg_user" id="reg_user" type="submit">Register</button>
                 
 
                   <div class="text-center">
